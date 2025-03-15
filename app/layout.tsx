288a3661f92a913047bd8442c26f3226fa0fc1e7
sofiatabioye <abioye.sofiat@outlook.com>
2025-03-15 12:11:49 +0000

    update page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import "./globals.css";
-import AuthProvider from "./providers/AuthProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>RoboVault</title> {/* Change this to your desired title */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+      </head>
       <body className="bg-gray-900 text-white">
-        {/* <AuthProvider> */}
-          {children}
-        {/* </AuthProvider> */}
+        {children}
       </body>
     </html>
   );
