ee25bd1ed1644cabb6ac677fc651c6152a264671
sofiatabioye <abioye.sofiat@outlook.com>
2025-03-15 12:55:03 +0000

    update email sender

--- lib/email.ts
+++ lib/email.ts
@@ -4,9 +4,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(to: string, token: string) {
   const verificationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/verify-email?token=${token}`;
-    console.log(verificationLink, to)
+
     await resend.emails.send({
-      from: "mobace.org",
+      from: "no-reply@robovault.com",
       to,
       subject: "Verify Your Email - Mobace",
       html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`,
